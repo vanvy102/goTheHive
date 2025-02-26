42bea4626326df1f7f16b6108ccf8fd335641b0c
vanvy102 <38358643+vanvy102@users.noreply.github.com>
2025-02-26 16:25:21 +0700

    Update Create_Case.go

--- Create_Case.go
+++ Create_Case.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -8,55 +9,19 @@ import (
 	"github.com/bitly/go-simplejson"
 )
 
-func CreateCase() {
-	hive := thehive.CreateLogin("http://127.0.0.1:9000/thehive", "dxA/NEvjl/hjcvv5rQ9aG/juLExgrHn6", false)
-
-	fmt.Println("Create Case")
-	fmt.Println("--------------------------")
-	response, err := hive.CreateCase(
-		"hive4go",  // Title
-		"N/A",      // Description
-		1,          // TLP
-		1,          // Severity
-		[]string{}, // Tags
-		true,
-	)
-
-	if err != nil {
-		fmt.Println("Error creating case:", err)
-		os.Exit(1)
-	}
-
-	jsonData, err := simplejson.NewJson(response.Raw)
-	if err != nil {
-		fmt.Println("Error parsing response:", err)
-		os.Exit(1)
-	}
-
-	ret, _ := jsonData.EncodePretty()
-	fmt.Println(string(ret))
-
-	id := jsonData.Get("id").MustString()
-	fmt.Printf("Get created case %s\n", id)
-	fmt.Println("--------------------------")
-
-	caseResponse, err := hive.GetCase(id)
-	if err != nil {
-		fmt.Println("Error getting case:", err)
-		os.Exit(1)
-	}
-
-	jsonData, err = simplejson.NewJson(caseResponse.Raw)
-	if err != nil {
-		fmt.Println("Error parsing case response:", err)
-		os.Exit(1)
-	}
-
-	ret, _ = jsonData.EncodePretty()
-	fmt.Println(string(ret))
-}
-
-// Hàm main để chạy chương trình
 func main() {
-	CreateCase()
+	hive := thehive.CreateLogin("http://10.11.161.24:9000", "R1xbVBu7RZh/C9xa6nsePkuNvabKlp2S", false)
+	query := `{"query": {"_in": {"_field": "title", "_values": ["Domain"]}}}`
+	resp, err := hive.FindCases([]byte(query))
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(resp)
+		os.Exit(1)
+	}
+	temp, _ := json.Marshal(resp.Detail)
+	fmt.Println(string(temp))
+	jsondata, err := simplejson.NewJson(resp.Raw)
+	fmt.Println(jsondata)
+	ret, _ := jsondata.EncodePretty()
+	fmt.Println(string(ret))
 }
